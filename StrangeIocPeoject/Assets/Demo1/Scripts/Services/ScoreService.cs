a2e40908dec757eb6ce42e5fd0cd6be00c48006f
DESKTOP-4B45D5T\hejun <517358323@qq.com>
2018-04-28 11:48:53 +0800

    注视

--- StrangeIocPeoject/Assets/Demo1/Scripts/Services/ScoreService.cs
+++ StrangeIocPeoject/Assets/Demo1/Scripts/Services/ScoreService.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class ScoreServide : IScoreService
 {
+    /// <summary>
+    /// 收到服务器端发来的分数
+    /// </summary>
     public void OnReceiveScore()
     {
         int score=Random .Range(20,100);
     }
-
+    /// <summary>
+    /// 请求分数
+    /// </summary>
+    /// <param name="url"></param>
     public void RequsetScore(string url)
     {
         Debug.Log("Request score form url:"+url);
 		OnReceiveScore();
     }
-
+    /// <summary>
+    /// 跟新服务端分数
+    /// </summary>
+    /// <param name="url"></param>
+    /// <param name="score"></param>
     public void UpdateScore(string url, int score)
     {
         Debug.Log("Update url"+url);
